833b571217f67af5a25af18f6ebc31b23340f4ae
Mahendra <mahendragehlot005@gmail.com>
2022-04-13 15:55:32 +0530

    part a completed

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
 
-const signUpmodal = document.getElementById('signUpModal');
+const signUpModal = document.getElementById('signUpModal');
 const signUpBtn = document.getElementById('signUpBtn');
 const closeSpan = document.getElementsByClassName('close')[0];
 
@@ -7,39 +7,54 @@ const signInModal = document.getElementById('signInModal');
 const signInBtn = document.getElementById('signInBtn');
 
 const singInclose = document.getElementById('singInclose');
-const singUpclose = document.getElementById('singUpclose')
+const singUpclose = document.getElementById('singUpclose');
+
+const createPostModal = document.getElementById('createPostModal');
+const createPostModalClose = document.getElementById('createPostModalclose');
+const createBtn = document.getElementById('createBtn');
 
 
 // open sign up modal
 
 signUpBtn.onclick = () => {
-    signUpmodal.style.display = 'block';
+    signUpModal.style.display = 'block';
     signInModal.style.display = 'none';
 }
 
 // open sign in modal
 
 signInBtn.onclick = () => {
-    signUpmodal.style.display = 'none';
+    signUpModal.style.display = 'none';
     signInModal.style.display = 'block';
 }
 
 // close by clicking on x
 
 singUpclose.addEventListener('click', () => {
-    signUpmodal.style.display = 'none';
+    signUpModal.style.display = 'none';
 });
 
 singInclose.addEventListener('click', () => {
     signInModal.style.display = 'none';
 });
 
+createBtn.addEventListener('click', () => {
+    createPostModal.style.display = 'block'
+});
+
+createPostModalClose.addEventListener('click', () => {
+    createPostModal.style.display = 'none';
+    console.log('create modla closed');
+});
+
 // close modal by clickng backdrop
 
 window.onclick = (event) => {
-    if (event.target == signInModal || event.target == signUpmodal) {
+    if (event.target == signInModal || event.target == signUpModal || event.target == createPostModal) {
         signInModal.style.display = "none";
         signUpModal.style.display = "none";
+        createPostModal.style.display = 'none';
+        //  console.log('window');
     }
 }
 
@@ -51,4 +66,8 @@ signUpPTag.addEventListener('click', () => {
     signInModal.style.display = "none";
     signUpModal.style.display = "block";
 
+});
+
+createPostModalClose.addEventListener('click', () => {
+    createPostModal.style.display = 'none';
 });
